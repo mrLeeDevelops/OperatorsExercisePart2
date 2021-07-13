557ab57df06f18c2237f374d467ae94e2e660b3c
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-07-12 22:59:01 -0500

    Initial Commit

---
+++ OperatorsExercisePart2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorsExercisePart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {AreaOfCircle((int)radius)}.");
+        }
+
+        public static double AreaOfCircle(int radius)
+        {
+            double area = (Math.PI * radius * radius);
+            return area;
+        }
+    }
+}
